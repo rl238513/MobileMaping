6c6d6c20089d96b8480e44b3332e739e5742bae6
EDUARDO MENDOZA <rl238513@rlas-macair-25.rlas-116.org>
2019-04-12 11:17:08 -0500

    Initial Commit

---
+++ MobileMaping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileMaping
+//
+//  Created by EDUARDO MENDOZA on 4/12/19.
+//  Copyright © 2019 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
